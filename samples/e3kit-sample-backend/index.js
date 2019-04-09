b1f2d9efc4bb2fb89b9cbb4bd790e39e22e25c37
Alexey Smirnov <alexey@smirnov.works>
2019-04-09 14:33:42 +0300

    delete umd

--- samples/e3kit-sample-backend/index.js
+++ samples/e3kit-sample-backend/index.js
@@ -1,14 +1,31 @@
-import getEThreeInstance from './snippets';
+import { Device } from './device';
 
-let sdk;
+const aliceDevice = new Device('Alice');
+const bobDevice = new Device('Bob');
 
-getEThreeInstance(prompt('Write here unique identity')).then(client => sdk = client)
-.then(() => sdk.hasLocalPrivateKey())
-.then(() => sdk.register())
-.then(() => sdk.encrypt('success!'))
-.then((encryptedMessage) => sdk.decrypt(encryptedMessage))
-.then((message) => alert(message))
-.then(() => sdk.cleanup())
-.catch((error) => {
-    console.error(error)
-});
+document.body.innerHTML = 'All magic in the code and results are in console';
+
+Promise.all([
+    aliceDevice.initialize(),
+    bobDevice.initialize()
+]).then(async ([aliceEThreeKit, bobEThreeKit]) => {
+    const aliceHasKey = await aliceEThreeKit.hasLocalPrivateKey();
+    const bobHasKey = await bobEThreeKit.hasLocalPrivateKey();
+    if (!aliceHasKey) {
+        await aliceEThreeKit.register();
+        await aliceDevice.eThree.backupPrivateKey('MySuperSecurePassword');
+    }
+    if (!bobHasKey) await bobEThreeKit.register();
+}).then(async () => {
+    const encryptedMessage = await aliceDevice.encryptMessage('Bob', 'Hi, Bob, How are you?');
+    console.log('encryptedMessage', encryptedMessage);
+
+    const decryptedMessage = await bobDevice.decryptMessage('Alice', encryptedMessage);
+    console.log('decryptedMessage', decryptedMessage);
+}).then(async () => {
+    await aliceDevice.eThree.cleanup();
+    console.log('Has Alice local private key', await aliceDevice.eThree.hasLocalPrivateKey())
+    await aliceDevice.login('MySuperSecurePassword')
+}).then(async () => {
+    console.log('Has Alice local private key after restore', await aliceDevice.eThree.hasLocalPrivateKey())
+})
