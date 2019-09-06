8ef1955fd595630b7eacc42eb1561f33ac2c4627
Vadim Avdeev <v.avdeiev@gmail.com>
2019-09-07 00:12:45 +0300

    fix checking if wasm modules are loaded; fix super call error

--- src/EThree.ts
+++ src/EThree.ts
@@ -39,6 +39,62 @@ import {
     DecryptFileOptions,
 } from './types';
 
+function cryptoModulesLoaded(getFn: () => any) {
+    try {
+        getFn();
+    } catch (_) {
+        return false;
+    }
+    return true;
+}
+
+function prepareBaseConstructorParams(identity, options: EThreeCtorOptions) {
+    const opts = withDefaults(options, {
+        apiUrl: DEFAULT_API_URL,
+        storageName: STORAGE_NAME,
+        useSha256Identifiers: false,
+    });
+
+    const accessTokenProvider = opts.accessTokenProvider;
+
+    const keyEntryStorage = opts.keyEntryStorage || new KeyEntryStorage(opts.storageName);
+    const virgilCrypto = new VirgilCrypto({ useSha256Identifiers: opts.useSha256Identifiers });
+    const cardCrypto = new VirgilCardCrypto(virgilCrypto);
+    const brainKeyCrypto = new VirgilBrainKeyCrypto();
+
+    const cardVerifier = new VirgilCardVerifier(cardCrypto, {
+        verifySelfSignature: opts.apiUrl === DEFAULT_API_URL,
+        verifyVirgilSignature: opts.apiUrl === DEFAULT_API_URL,
+    });
+
+    const keyLoader = new PrivateKeyLoader(identity, {
+        accessTokenProvider,
+        virgilCrypto,
+        brainKeyCrypto,
+        keyEntryStorage,
+        apiUrl: opts.apiUrl,
+    });
+
+    const cardManager = new CardManager({
+        cardCrypto,
+        cardVerifier,
+        accessTokenProvider,
+        retryOnUnauthorized: true,
+        apiUrl: opts.apiUrl,
+    });
+
+    return {
+        identity,
+        virgilCrypto,
+        cardCrypto,
+        cardVerifier,
+        cardManager,
+        accessTokenProvider,
+        keyEntryStorage,
+        keyLoader,
+    };
+}
+
 export class EThree extends AbstractEThree {
     /**
      * @hidden
@@ -46,50 +102,7 @@ export class EThree extends AbstractEThree {
      */
     // @ts-ignore
     constructor(identity: string, options: EThreeCtorOptions) {
-        const opts = withDefaults(options, {
-            apiUrl: DEFAULT_API_URL,
-            storageName: STORAGE_NAME,
-            useSha256Identifiers: false,
-        });
-
-        const accessTokenProvider = opts.accessTokenProvider;
-
-        const keyEntryStorage = opts.keyEntryStorage || new KeyEntryStorage(opts.storageName);
-        const virgilCrypto = new VirgilCrypto({ useSha256Identifiers: opts.useSha256Identifiers });
-        const cardCrypto = new VirgilCardCrypto(virgilCrypto);
-        const brainKeyCrypto = new VirgilBrainKeyCrypto();
-
-        const cardVerifier = new VirgilCardVerifier(cardCrypto, {
-            verifySelfSignature: opts.apiUrl === DEFAULT_API_URL,
-            verifyVirgilSignature: opts.apiUrl === DEFAULT_API_URL,
-        });
-
-        const keyLoader = new PrivateKeyLoader(identity, {
-            accessTokenProvider,
-            virgilCrypto,
-            brainKeyCrypto,
-            keyEntryStorage,
-            apiUrl: opts.apiUrl,
-        });
-
-        const cardManager = new CardManager({
-            cardCrypto,
-            cardVerifier,
-            accessTokenProvider,
-            retryOnUnauthorized: true,
-            apiUrl: opts.apiUrl,
-        });
-
-        super({
-            identity,
-            virgilCrypto,
-            cardCrypto,
-            cardVerifier,
-            cardManager,
-            accessTokenProvider,
-            keyEntryStorage,
-            keyLoader,
-        });
+        super(prepareBaseConstructorParams(identity, options));
     }
 
     /**
@@ -101,10 +114,10 @@ export class EThree extends AbstractEThree {
         options: EThreeInitializeOptions = {},
     ): Promise<EThree> {
         const modulesToLoad: Promise<void>[] = [];
-        if (!getFoundationModules()) {
+        if (!cryptoModulesLoaded(getFoundationModules)) {
             modulesToLoad.push(initFoundation().then(setFoundationModules));
         }
-        if (!getPythiaModules()) {
+        if (!cryptoModulesLoaded(getPythiaModules)) {
             modulesToLoad.push(initPythia());
         }
         await Promise.all(modulesToLoad);
